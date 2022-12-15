efc1150d051734f8b6e14ffa25b08cecb12aa8e0
Abhishek Khatri <abhishekkhatri98@gmail.com>
2022-12-14 20:53:34 -0500

    feat: word search complete

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const path = require("path");
 const textCleaner = require("text-cleaner");
 const { htmlToText } = require("html-to-text");
 const { removeStopwords } = require("stopword");
-const { buildTrie, search } = require("./trie");
+const readline = require("readline").createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+const Trie = require("./trie");
+const { resolve } = require("path");
 
 const filesDirectory = path.resolve("data");
 
@@ -36,20 +41,35 @@ const readFiles = async (files) => {
 	return files;
 };
 
+const getUserInput = async () =>
+	new Promise((resolve) => {
+		readline.question(
+			"\n\nEnter the word you want to searh (Enter ':q' to quit): ",
+			resolve
+		);
+	});
+
 const main = async () => {
 	try {
 		console.log(`Reading files from: ${filesDirectory}`);
 		let files = await fs.readdir(filesDirectory);
 		files = await readFiles(files);
-		const trieMap = buildTrie(files);
-		const result = search(trieMap, "cristiano");
-		console.log();
-		console.log("Search Result: ");
-		const resultArr = Object.entries(result).map((entry) => ({
-			"File Name": entry[0],
-			Occurrence: entry[1],
-		}));
-		console.table(resultArr);
+		const trie = new Trie(files);
+		let userInput = await getUserInput();
+		while (userInput !== ":q") {
+			const result = trie.search(userInput);
+			console.log("\nSearch Result: ");
+			const resultArr = Object.entries(result)
+				.sort((a, b) => b[1] - a[1])
+				.map((entry) => ({
+					"File Name": entry[0],
+					Occurrence: entry[1],
+				}));
+			if (resultArr.length > 0) console.table(resultArr);
+			else console.log("Not found");
+			userInput = await getUserInput();
+		}
+		readline.close();
 	} catch (err) {
 		console.error(err);
 	}
--- trie.js
+++ trie.js
@@ -4,50 +4,59 @@ const createNode = () => {
 	return node;
 };
 
-const root = createNode();
-
-const buildTrie = (files) => {
-	const processWord = (fileName, word) => {
-		let current = root;
-
-		word.split("").forEach((char) => {
-			let occurrence = 0;
-			if (!current.has(char)) current.set(char, createNode());
-			current = current.get(char);
-			if (current.get("results").has(fileName))
-				occurrence = current.get("results").get(fileName);
-			current.get("results").set(fileName, occurrence + 1);
-		});
-	};
-
-	const processTextSnippet = (fileName, fileData) => {
-		fileData.forEach((word) => {
-			processWord(fileName, word);
-		});
-	};
-
-	files.forEach((file) => {
-		processTextSnippet(file.file, file.data);
-	});
-
-	return root;
-};
-
-const renderSearchResults = (resultMap) => {
-	return Object.fromEntries(resultMap);
-};
-
-const search = (trieMap, text) => {
-	for (const letter of text.toLowerCase()) {
-		if (!trieMap.has(letter)) {
-			return renderSearchResults(new Set());
-		}
-		trieMap = trieMap.get(letter);
+class Trie {
+	#root = createNode();
+	constructor(files) {
+		this.#buildTrie(files);
 	}
-	return renderSearchResults(trieMap.get("results"));
-};
 
-module.exports = {
-	buildTrie,
-	search,
-};
+	#buildTrie = (files) => {
+		const processWord = (fileName, word) => {
+			let current = this.#root;
+
+			word.split("").forEach((char) => {
+				let occurrence = 0;
+				if (!current.has(char)) current.set(char, createNode());
+				current = current.get(char);
+				if (current.get("results").has(fileName))
+					occurrence = current.get("results").get(fileName);
+				current.get("results").set(fileName, occurrence + 1);
+			});
+		};
+
+		const processTextSnippet = (fileName, fileData) => {
+			fileData.forEach((word) => {
+				processWord(fileName, word);
+			});
+		};
+
+		files.forEach((file) => {
+			processTextSnippet(file.file, file.data);
+		});
+	};
+
+	getTrie = () => {
+		return this.#root;
+	};
+
+	search = (text) => {
+		const renderSearchResults = (resultMap) => {
+			return Object.fromEntries(resultMap);
+		};
+		let trieMap = this.#root;
+		text = text
+			.split("")
+			.map((char) => char.trim().toLowerCase())
+			.filter((char) => char !== "")
+			.join("");
+		for (const letter of text.toLowerCase()) {
+			if (!trieMap.has(letter)) {
+				return renderSearchResults(new Map());
+			}
+			trieMap = trieMap.get(letter);
+		}
+		return renderSearchResults(trieMap.get("results"));
+	};
+}
+
+module.exports = Trie;
